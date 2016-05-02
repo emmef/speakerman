9b836c00c88bb38567ad65898fbf325e81b1c317
Emmef <the.emmef@gmail.com>
2016-05-03 00:30:41 +0200

    Much more intuitive display of levels and limiting

--- src/web/speakerman.js
+++ src/web/speakerman.js
@@ -5,15 +5,31 @@ function createCORSRequest(method, url) {
 }
 
 var xHttpRequest = null;
+var subOldValues = { previousValue: 0 };
+var limitMinGain = 0.25;
+var limitPower = 0.99;
+var signalMinGain = 3e-4;
+var signalPower = 2;
+var minimumColorPercentage = 0.02;
 
-function getSignalInDbPercent(level, minLvl) {
+function getSignalInDbPercent(level, minLvl, pow) {
 	var minLevel = Math.max(minLvl, 1e-6);
 	var absLevel = Math.abs(level);
 	var level = Math.max(minLevel, absLevel);
 	var minLog = Math.abs(Math.log(minLevel));
 	var levelLog = Math.log(level);
+	if (pow) {
+		pow = Math.max(0.25, Math.min(4, pow));
+	}
+	else {
+		pow = 1
+	}
+	var result = (levelLog + minLog) / minLog;
+	if (pow < 1) {
+		result = 1 - result;
+	}
 
-	return (levelLog + minLog) / minLog;
+	return Math.pow(result, pow);
 }
 
 function scaledValue(value, scale) {
@@ -24,7 +40,7 @@ function scaledValue(value, scale) {
 
 function getRgbValue(scale, r, g, b, minNonZero) {
 	var boundScale = Math.min(1.0, Math.max(0.0, typeof scale === 'number' ? scale : 0));
-	var threshold = (!minNonZero) ? 0 : typeof minNonZero != 'number' ? 0.25 : Math.min(0.5, Math.max(0, minNonZero));
+	var threshold = (!minNonZero) ? 0 : typeof minNonZero != 'number' ? minimumColorPercentage : Math.min(0.5, Math.max(0, minNonZero));
 	var usedScale = boundScale < 0.002 ? 0 : threshold + (1.0 - threshold) * boundScale;
 	
 	return "rgb(" 
@@ -33,12 +49,35 @@ function getRgbValue(scale, r, g, b, minNonZero) {
 		+ scaledValue(b, usedScale) + ")";
 }
 
+function integrate(element, val, perc) {
+	var percentage = Math.max(0, Math.min(1, perc));
+	var value = Math.max(0, Math.min(1, val));
+	if (element) {
+		if (element.previousValue) {
+			if (value > element.previousValue) {
+				element.previousValue = value;
+			}
+			else {
+				element.previousValue = percentage * value + (1 - percentage) * element.previousValue;
+			}
+		}
+		else {
+			element.previousValue = value;
+		}
+		return element.previousValue;
+	}
+	return val;
+}
+
 function setMeters(levels) 
 {
 	var i;
 	for (i = 0; i < 4; i++) {
-		var subGain = levels.subGain;
-		var subGainAverage = levels.subGainAverage;
+		var subGainAverage = integrate(subOldValues, levels.subGainAverage, 0.1);
+		var subGain = Math.min(levels.subGain, subGainAverage);
+		var subAvgGainPercentage = getSignalInDbPercent(subGainAverage, limitMinGain, limitPower);
+		var subGainPercentage = getSignalInDbPercent(subGain, limitMinGain, limitPower);
+		
 		if (levels.group && levels.group[i]) {
 			var group = levels.group[i];
 			var prefix = "group_" + i;
@@ -46,64 +85,30 @@ function setMeters(levels)
 			var sub = document.getElementById(prefix + "_sub");
 			var mainAvg = document.getElementById(prefix + "_main_avg");
 			var main = document.getElementById(prefix + "_main");
-
-			var signalPercentage = group.level > 1e-3 ? getSignalInDbPercent(group.level, 1e-2) : 0; 
+			var signal = document.getElementById(prefix + "_signal");
 			
-			var subAvgGainPercentage = 1 - getSignalInDbPercent(subGainAverage, 0.25);
-			var subGainPercentage = Math.max(subAvgGainPercentage, 1 - getSignalInDbPercent(subGain, 0.25));
-			var mainAvgGainPercentage = 1 - getSignalInDbPercent(group.gainAverage, 0.25);
-			var mainGainPercentage = Math.max(mainAvgGainPercentage, 1 - getSignalInDbPercent(group.gain, 0.25));
-			var mainGainMax = Math.max(subGainPercentage, mainGainPercentage);
+			var mainGainAverage = integrate(mainAvg, group.gainAverage, 0.1);
+			var mainGain = Math.min(group.gain, mainGainAverage);
+			var signalValue = integrate(signal, group.level, 0.8) * Math.min(subGainAverage, mainGainAverage);
+			
+			var signalPercentage = getSignalInDbPercent(signalValue, signalMinGain, signalPower); 
 			
 			if (subAvg) {
-				if (subAvgGainPercentage > 0.01) {
-					subAvg.style.backgroundColor = getRgbValue(subAvgGainPercentage, 255, 0, 64, 0.4);
-				}
-				else if (mainGainMax > 0.01) {
-					subAvg.style.backgroundColor = "black";
-				}
-				else {
-					subAvg.style.backgroundColor = getRgbValue(signalPercentage, 0, 255, 0, 0.2);
-				}
+				subAvg.style.backgroundColor = getRgbValue(subAvgGainPercentage, 255, 0, 64, minimumColorPercentage);
 			}
 			if (sub) {
-				if (subGainPercentage > 0.01) {
-					sub.style.backgroundColor = getRgbValue(Math.max(subGainPercentage, subAvgGainPercentage), 255, 0, 64, 0.4);
-				}
-				else if (subAvgGainPercentage > 0.01) {
-					sub.style.backgroundColor = getRgbValue(subAvgGainPercentage, 255, 0, 64, 0.4);
-				}
-				else if (mainGainMax > 0.01) {
-					sub.style.backgroundColor = "black";
-				}
-				else {
-					sub.style.backgroundColor = getRgbValue(signalPercentage, 0, 255, 0, 0.2);
-				}
+				sub.style.backgroundColor = getRgbValue(subGainPercentage, 255, 0, 64, minimumColorPercentage);
 			}
 			if (mainAvg) {
-				if (mainAvgGainPercentage > 0.01) {
-					mainAvg.style.backgroundColor = getRgbValue(mainAvgGainPercentage, 255, 0, 0, 0.4);
-				}
-				else if (mainGainMax > 0.01) {
-					mainAvg.style.backgroundColor = "black";
-				}
-				else {
-					mainAvg.style.backgroundColor = getRgbValue(signalPercentage, 0, 255, 0, 0.2);
-				}
+				var mainAvgGainPercentage = getSignalInDbPercent(mainGainAverage, limitMinGain, limitPower);
+				mainAvg.style.backgroundColor = getRgbValue(mainAvgGainPercentage, 255, 0, 0, minimumColorPercentage);
 			}
 			if (main) {
-				if (mainGainPercentage > 0.01) {
-					main.style.backgroundColor = getRgbValue(mainGainPercentage, 255, 0, 0, 0.4);
-				}
-				else if (mainAvgGainPercentage > 0.01) {
-					main.style.backgroundColor = getRgbValue(mainAvgGainPercentage, 255, 0, 0, 0.4);
-				}
-				else if (mainGainMax > 0.01) {
-					main.style.backgroundColor = "black";
-				}
-				else {
-					main.style.backgroundColor = getRgbValue(signalPercentage, 0, 255, 0, 0.2);
-				}
+				var mainGainPercentage = getSignalInDbPercent(mainGain, limitMinGain, limitPower);
+				main.style.backgroundColor = getRgbValue(mainGainPercentage, 255, 0, 0, minimumColorPercentage);
+			}
+			if (signal) {
+				signal.style.backgroundColor = getRgbValue(signalPercentage, 0, 255, 0, minimumColorPercentage);
 			}
 		}
 		else {
@@ -141,7 +146,7 @@ function sendLevelRequest()
 			xHttpRequest.onload = handleRequest;
 			xHttpRequest.onloadend = function() {
 				xHttpRequest = null;
-				window.setTimeout(function() {sendLevelRequest(); }, 50);					
+				window.setTimeout(function() {sendLevelRequest(); }, 75);
 			}
 			xHttpRequest.send();
 		}
