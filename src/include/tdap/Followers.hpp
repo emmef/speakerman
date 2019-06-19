93a8bcadde535ca1ec49b2b7fb2dbf44422283e8
Emmef <the.emmef@gmail.com>
2019-06-20 01:58:23 +0200

    Rectangular peak detection finally correct; need to find missing piece in growing peaks

--- src/include/tdap/Followers.hpp
+++ src/include/tdap/Followers.hpp
@@ -24,63 +24,28 @@
 #define TDAP_FOLLOWERS_HEADER_GUARD
 
 #include <tdap/Integration.hpp>
+#ifdef TDAP_FOLLOWERS_DEBUG_LOGGING
+#include <cstdio>
+#define TDAP_FOLLOWERS_INFO(...) std::printf(__VA_ARGS__)
+#else
+#define TDAP_FOLLOWERS_INFO(...)
+#endif
+
+#if TDAP_FOLLOWERS_DEBUG_LOGGING > 1
+#define TDAP_FOLLOWERS_DEBUG(...) std::printf(__VA_ARGS__)
+#else
+#define TDAP_FOLLOWERS_DEBUG(...)
+#endif
+
+#if TDAP_FOLLOWERS_DEBUG_LOGGING > 2
+#define TDAP_FOLLOWERS_TRACE(...) std::printf(__VA_ARGS__)
+#else
+#define TDAP_FOLLOWERS_TRACE(...)
+#endif
 
 namespace tdap {
 
     using namespace std;
-
-/**
- * Follows rises in input and holds those for a number of samples. After that,
- * follow the (lower) input.
- */
-//    template<typename S>
-//    class HoldMax
-//    {
-//        static_assert(is_arithmetic<S>::value,
-//                      "Sample type S must be arithmetic");
-//        size_t holdSamples;
-//        size_t toHold;
-//        S holdValue;
-//
-//    public:
-//        HoldMax(size_t holdForSamples, S initialHoldValue = 0) :
-//                holdSamples(holdForSamples), toHold(0),
-//                holdValue(initialHoldValue)
-//        {}
-//
-//        void resetHold()
-//        {
-//            toHold = 0;
-//        }
-//
-//        void setHoldCount(size_t newCount)
-//        {
-//            holdSamples = newCount;
-//            if (toHold > holdSamples) {
-//                toHold = holdSamples;
-//            }
-//        }
-//
-//        size_t getHoldSamples() const
-//        {
-//            return holdSamples;
-//        }
-//
-//        S apply(S input)
-//        {
-//            if (input > holdValue) {
-//                toHold = holdSamples;
-//                holdValue = input;
-//                return holdValue;
-//            }
-//            if (toHold > 0) {
-//                toHold--;
-//                return holdValue;
-//            }
-//            return input;
-//        }
-//    };
-
 /**
  * Follows rises in input and holds those for a number of samples. After that,
  * follows the (lower) input in an integrated fashion.
@@ -305,6 +270,404 @@ namespace tdap {
         {
             return integrator_;
         }
+    };
+
+    template<typename S>
+    class TriangularFollower
+    {
+        class Node {
+            size_t position_;
+            S value_;
+            S delta_;
+
+            S projectedValue(ssize_t pointPosition) const
+            {
+                return value_ + delta_ * (pointPosition - static_cast<ssize_t>(position_));
+            }
+        public:
+            /**
+             *Constructs a new node with the exact given values.
+             * @param pos The position of the Node
+             * @param v The value of the Node
+             * @param d The delta before the position of the node
+             */
+            Node(size_t pos, S v, S d) :
+            position_(pos),
+            value_(v),
+            delta_(d)
+            {}
+            Node() : Node(0,1,1) {}
+            /**
+             * Constructs a node from the current situation to the given one.
+             *
+             * @param currentPosition The position where we want to construct route to node
+             * @param currentValue The (detected) value at the current position
+             * @param newPosition The position of the Node
+             * @param newValue The value at the new position
+             */
+            Node(size_t currentPosition, S currentValue, size_t newPosition, S newValue) :
+                position_(newPosition),
+                value_(newValue),
+                delta_((newValue - currentValue) / (1 + newPosition - currentPosition))
+            {
+                TDAP_FOLLOWERS_TRACE("# \t\t\t Node from (%zu, %lf) -> (%zu, %lf)\n",
+                        currentPosition, currentValue, newPosition, newValue);
+            }
+            size_t position() const { return position_; }
+            S value() const { return value_; }
+            S delta() const { return delta_; }
+            /**
+             * Projects the value of the point position, using the node properties.
+             * @param pointPosition The point position
+             * @return the projected value
+             */
+            inline S project(size_t pointPosition) const
+            {
+                TDAP_FOLLOWERS_TRACE("# \t\t ");
+                print();
+                S result = projectedValue(pointPosition);
+                TDAP_FOLLOWERS_TRACE(
+                        ".project(%zu) = %lf\n",
+                        pointPosition, result);
+                return result;
+            }
+
+            /**
+             * Projects the value of this node's position, projected from the
+             * other node's properties.
+             * @param other The other node.
+             * @return the projected value
+             */
+            inline S projectedFrom(const Node &other) const
+            {
+                TDAP_FOLLOWERS_TRACE("# \t\t ");
+                print();
+                TDAP_FOLLOWERS_TRACE(".projectedFrom(");
+                other.print();
+                S result = other.projectedValue(position_);
+                TDAP_FOLLOWERS_TRACE(")=%lf\n", result);
+                return result;
+            }
+            inline void print() const
+            {
+                print(position_, value_, delta_);
+            }
+            static inline void print(size_t position, S value, S delta)
+            {
+                TDAP_FOLLOWERS_TRACE(
+                        "{position=%zu, value=%lf, delta=%lf}",
+                        position, value, delta);
+
+            }
+        };
+
+        class NodeManager
+        {
+            size_t maxNodes_;
+            Node * const node_;
+            size_t count_;
+            size_t start_;
+
+            size_t validMaxNodes(size_t maxNodes)
+            {
+                if (maxNodes < 2) {
+                    throw invalid_argument("TriangularFollower::NodeManager::<init>: Number of nodes must be larger than 2.");
+                }
+                if (maxNodes > Count<Node>::max()) {
+                    throw invalid_argument("TriangularFollower::NodeManager::<init>: Number of nodes exceeds maximum.");
+                }
+                return maxNodes;
+            }
+            inline size_t fromFirstIndex(size_t index) const
+            {
+                return start_ + IndexPolicy::array(index, count_);
+            }
+            inline size_t lastIndex() const
+            {
+                return start_ + count_ - 1;
+            }
+            inline size_t fromLastIndex(size_t index) const
+            {
+                return lastIndex() - IndexPolicy::array(index, count_);
+            }
+        public:
+            NodeManager(size_t maxNodes)
+            :
+                maxNodes_(validMaxNodes(maxNodes)),
+                node_(new Node[maxNodes_]),
+                count_(0),
+                start_(0)
+                {}
+
+            inline size_t count() const { return count_; }
+            inline bool hasNodes() const { return count_ > 0; }
+            inline const Node *first() const { return count_ > 0 ? node_ + start_: nullptr; }
+            inline const Node *last() const { return count_ > 0 ? node_ + start_ + count_ - 1 : nullptr; }
+            inline const Node *fromFirst(size_t index) const
+            {
+                if (count_ > 0) {
+                    return node_ + fromFirstIndex(index);
+                }
+                return nullptr;
+            }
+            inline const Node *fromLast(size_t index) const
+            {
+                return count_ > 0 ? node_ + fromLastIndex(index) : nullptr;
+            }
+            inline const Node *next()
+            {
+                if (count_ == 0) {
+                    throw runtime_error("TriangularFollower::NodeManager::add: No nodes to proceed to");
+                }
+                if (--count_ == 0) {
+                    start_ = 0;
+                }
+                else {
+                    start_++;
+                }
+                return first();
+            }
+            inline void reset()
+            {
+                TDAP_FOLLOWERS_TRACE("# \t TriangularFollower::NodeManager::reset()\n");
+                count_ = 0;
+                start_ = 0;
+            }
+            inline void add(size_t at, const Node &source)
+            {
+                if (at > count_) {
+                    throw runtime_error("TriangularFollower::NodeManager::add: Number of nodes exceeded: cannot create new one");
+                }
+                count_ = at;
+                if (count_ == 0) {
+                    start_ = 0;
+                }
+                add(source);
+            }
+            inline void add(const Node &source)
+            {
+                if (count_ >= maxNodes_) {
+                    throw runtime_error("TriangularFollower::NodeManager::add: Number of nodes exceeded: cannot create new one");
+                }
+                TDAP_FOLLOWERS_TRACE("# \t TriangularFollower::NodeManager::add(");
+                source.print();
+                node_[count_++] = source;
+                TDAP_FOLLOWERS_TRACE(") { count=%zu; }\n", count_);
+            }
+            inline bool isInReleaseOrIdle() const
+            {
+                bool result = count_ <= 1;
+                TDAP_FOLLOWERS_TRACE("# \t isInReleaseOrIdle() = %i\n", result);
+                return result;
+            }
+            inline bool isBelowReleaseEnvelope(size_t newSamplePtr, const S newPeakValue) const
+            {
+                TDAP_FOLLOWERS_TRACE("# \t isBelowReleaseEnvelope(new-peak-at=%zu, new-peak-value=%lf)\n", newSamplePtr, newPeakValue);
+                S projected = last()->project(newSamplePtr);
+                bool result = newPeakValue <= projected;
+                TDAP_FOLLOWERS_TRACE("# \t isBelowReleaseEnvelope = %i { nodes=%zu; }\n", result, count());
+                return result;
+            }
+            inline bool isBelowPeakAttack(const Node &newPeak, size_t lastIndex) const
+            {
+                TDAP_FOLLOWERS_TRACE("# \t isBelowPeakAttack(");
+                newPeak.print();
+                TDAP_FOLLOWERS_TRACE(", nodeIndex=%zu)", lastIndex);
+                Node *at = fromLast(lastIndex);
+                if (at == nullptr) {
+                    return false;
+                }
+                S projected = at->projectedFrom(newPeak);
+                bool result = at->value() < projected;
+                TDAP_FOLLOWERS_TRACE("# \t isBelowPeakAttack = %i\n", result);
+                return result;
+            }
+            inline ssize_t getLastAbovePeakAttackEnvelope(const Node &newPeak)
+            {
+                TDAP_FOLLOWERS_TRACE("# \tgetLastAbovePeakAttackEnvelope(");
+                newPeak.print();
+                if (count() < 2) {
+                    TDAP_FOLLOWERS_TRACE(") : first peak\n");
+                    return -1;
+                }
+                TDAP_FOLLOWERS_TRACE(") : search\n");
+                for (ssize_t index = fromLastIndex(1); index >= (ssize_t )start_; index--) {
+                    const Node * node = node_ + index;
+                    S nodeValue = node->value();
+                    TDAP_FOLLOWERS_TRACE("# \t\t (%zu) ", index);
+                    node->print();
+                    TDAP_FOLLOWERS_TRACE("\n");
+                    S projected = newPeak.project(node->position());
+                    if (nodeValue > projected) {
+                        TDAP_FOLLOWERS_TRACE("# \t\t FOUND\n");
+                        return index - start_;
+                    }
+                }
+                return -1;
+            }
+            ~NodeManager()
+            {
+                if (node_) {
+                    delete [] node_;
+                }
+            }
+        };
+
+        NodeManager nodes_;
+        S threshold_ = 1;
+        S detect_ = 0;
+        size_t position_ = 0;
+        size_t attSamples_ = 1;
+        size_t relSamples_ = 1;
+
+        S constructNewFirstPeak(const S value)
+        {
+            TDAP_FOLLOWERS_TRACE("# \t constructNewFirstPeak(value=%lf): reached towards node\n", value);
+            size_t newPtr = position_ + attSamples_ - 1;
+            nodes_.add(0, { position_, detect_, newPtr, value });
+            nodes_.add({newPtr + relSamples_, threshold_,
+                           (threshold_ - value) / relSamples_});
+            return detect_;//continueAsNormal();
+        }
+        S constructFromNode(const size_t nodePtr, const size_t position, const S value)
+        {
+            TDAP_FOLLOWERS_TRACE("# \t constructFromNode(nodePtr=%zu, position=%zu, value=%lf)\n", nodePtr, position, value);
+            if (nodePtr == 0) {
+                return constructNewFirstPeak(value);
+            }
+            const Node *from = nodes_.fromFirst(nodePtr);
+
+            nodes_.add(nodePtr + 1, { from->position(), from->value(), position, value});
+            nodes_.add({ position, value, position + relSamples_, threshold_ });
+            nodes_ = nodePtr + 2;
+            return continueAsNormal();
+        }
+
+        S continueAsNormal()
+        {
+            TDAP_FOLLOWERS_TRACE("# \t continueAsNormal()\n");
+            if (!nodes_.hasNodes()) {
+                TDAP_FOLLOWERS_TRACE("# \t\t nodes=0\n");
+                position_++; // Might want to reset to zero instead
+                return threshold_;
+            }
+            const Node *towards = nodes_.first();
+            TDAP_FOLLOWERS_TRACE("# \t\t detect=%lf, towards=", detect_);
+//            detect_ += towards->delta();
+            detect_ = towards->project(position_);
+            if (position_++ >= towards->position()) {
+                towards = nodes_.next();
+                if (towards == nullptr) {
+                    TDAP_FOLLOWERS_TRACE("# \t\t\t -- Last node encountered: reset\n");
+                    return detect_;
+                }
+                TDAP_FOLLOWERS_TRACE("# \t\t\t -- Move to next node\n");
+            }
+            return detect_;
+        }
+
+        inline S followAlgorithm(const S value)
+        {
+            if (value < threshold_) {
+                TDAP_FOLLOWERS_TRACE("# \t below threshold %lf\n", threshold_);
+                return continueAsNormal();
+            }
+            size_t newPtr = position_ + attSamples_;
+            if (nodes_.count() == 0) {
+                nodes_.add(0, { position_, threshold_, newPtr, value });
+                nodes_.add({ newPtr + 1, value, newPtr + relSamples_, threshold_ });
+                return continueAsNormal();
+            }
+            if (nodes_.isBelowReleaseEnvelope(newPtr, value)) {
+                return continueAsNormal();
+            }
+            if (nodes_.count() == 1) {
+                nodes_.add(0, { position_, detect_, newPtr, value });
+                nodes_.add({ newPtr + 1, value, newPtr + relSamples_, threshold_ });
+                return continueAsNormal();
+            }
+            const Node newPeak = { position_, threshold_, newPtr, value };
+            const Node newRelease = { newPtr + 1, value, newPtr + relSamples_, threshold_ };
+            ssize_t higherNode = nodes_.getLastAbovePeakAttackEnvelope(newPeak);
+            if (higherNode < 0) {
+                nodes_.add(0, { position_, detect_, newPtr, value});
+                nodes_.add(newRelease);
+            }
+            else {
+                const Node * from = nodes_.fromFirst(higherNode);
+                nodes_.add(higherNode + 1, {from->position() + 1, from->value(), newPeak.position(), newPeak.value()});
+                nodes_.add(newRelease);
+            }
+            return continueAsNormal();
+        }
+
+    public:
+        TriangularFollower(size_t maxNodes)
+        :
+            nodes_(maxNodes) {
+            TDAP_FOLLOWERS_DEBUG("# TriangularFollower(%zu)\n", maxNodes);
+        }
+
+        ~TriangularFollower()
+        {
+        }
+
+        inline S follow(const S value)
+        {
+            TDAP_FOLLOWERS_TRACE("### follow(%lf) // ptr=%zu, nodes=%zu\n", value, position_, nodes_.count());
+            S result = followAlgorithm(value);
+            TDAP_FOLLOWERS_TRACE("### follow = %lf\n", result);
+            return result;
+        }
+
+        void setTimeConstantAndSamples(size_t attackSamples, size_t releaseSamples, S threshold)
+        {
+            attSamples_ = Sizes::valid_positive(attackSamples);
+            relSamples_ = Sizes::valid_positive(releaseSamples);
+            threshold_ = threshold;
+            detect_ = threshold_;
+            nodes_.reset();
+            position_ = 0;
+        }
+    };
+
+    template<typename S>
+    class CompensatedAttack
+    {
+        S compensation = 1;
+        S previousPeak = 0;
+        S compensatedPeak = 0;
+        S detectedPeak = 0;
+        IntegrationCoefficients<S> coefficients;
+    public:
+
+        void setTimeConstantAndSamples(size_t timeConstantSamples, size_t samples, S initialValue)
+        {
+            coefficients.setCharacteristicSamples(timeConstantSamples);
+            S output = 0.0;
+            for (size_t sample = 0; sample < samples; sample++) {
+                output += coefficients.inputMultiplier() * (1.0 - output);
+            }
+            compensation = 1.0 / output;
+            previousPeak = initialValue;
+            compensatedPeak = initialValue;
+            detectedPeak = initialValue;
+        }
+
+        S follow(const S peak)
+        {
+            if (peak < previousPeak) {
+                compensatedPeak = peak;
+                previousPeak = peak;
+            }
+            else if (peak > previousPeak) {
+                compensatedPeak = detectedPeak + compensation * (peak - detectedPeak);
+                previousPeak = peak;
+            }
+            detectedPeak += coefficients.inputMultiplier() * (compensatedPeak - detectedPeak);
+            return detectedPeak;
+        }
+
     };
 
     template<typename sample_t>
