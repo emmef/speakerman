464f9fbd968d5dabbb36e6cfef96715aff425370
Emmef <the.emmef@gmail.com>
2021-11-01 22:23:04 +0100

    Fixed warning in level exertion watchdog script and added a test modus.

--- src/speakerman-watchdog.sh
+++ src/speakerman-watchdog.sh
@@ -1,26 +1,36 @@
 #!/usr/bin/env bash
 
-if [ -z "$1" ]; then
+
+checking=
+
+if [ "$1" == "--check" ]
+then
+  checking="yes"
+elif [ -z "$1" ]
+then
   echo "require output file"
   exit 1
-fi
-
-if ! echo "test" >"$1"; then
+elif ! echo "test" >"$1";
+then
   echo "Cannot write to output file"
   exit 1
+else
+  output_file="$1"
 fi
-OUTPUT_FILE="$1"
-
-PARENT="$1"
 
 URL="https://emmef.org/limiter/limiter.txt"
 
 exceeded_allowed() {
   #   $1 is "now" timestamp
-  local now=$(echo "$1" | egrep '^[0-9]{12}$')
+  local now
+  now=$(echo "$1" | grep -E '^[0-9]{12}$')
   if test -z "$now"; then
     echo "No or invalid current time-stamp: $1"
     return 1
+  fi
+  if [ -n "$checking" ]
+  then
+    echo "NOW = $now"
   fi
   local startstamp
   local endstamp
@@ -28,13 +38,13 @@ exceeded_allowed() {
   local line
   local expr_nodashes='^([0-9]{12})( *- *)([0-9]{12})( *: *)(1|2|3|4|5|6)(| .*)$'
   local expr_dashes='^([0-9]{4}-[0-9]{2}-[0-9]{2}(T|_| )[0-9]{2}:[0-9]{2})( *- *)([0-9]{4}-[0-9]{2}-[0-9]{2}(T|_| )[0-9]{2}:[0-9]{2})( *: *)(1|2|3|4|5|6)(| .*)$'
-  while read line; do
+  while read -r line; do
     line=$(echo "$line" | sed -r 's/^\s+(.*)$/\1/' | sed -r 's/^([^#]*)(#.*)*$/\1/')
-    if echo "$line" | egrep "$expr_nodashes" >/dev/null; then
+    if echo "$line" | grep -E "$expr_nodashes" >/dev/null; then
       startstamp=$(echo "$line" | sed -r "s/$expr_nodashes/\1/")
       endstamp=$(echo "$line" | sed -r "s/$expr_nodashes/\3/")
       level=$(echo "$line" | sed -r "s/$expr_nodashes/\5/")
-    elif echo "$line" | egrep "$expr_dashes" >/dev/null; then
+    elif echo "$line" | grep -E "$expr_dashes" >/dev/null; then
       startstamp=$(echo "$line" | sed -r "s/$expr_dashes/\1/" | tr -d 'T: \-_')
       endstamp=$(echo "$line" | sed -r "s/$expr_dashes/\4/" | tr -d 'T: \-_')
       level=$(echo "$line" | sed -r "s/$expr_dashes/\7/")
@@ -43,10 +53,20 @@ exceeded_allowed() {
       endstamp=
       level=
     fi
-    if echo "$startstamp-$endstamp:$level" | egrep '^[0-9]{12}-[0-9]{12}:(1|2|3|4|5|6)$' >/dev/null; then
-      if test "$now" -ge "$startstamp" && test "$now" -le "$endstamp"; then
-        echo "$level" >"$OUTPUT_FILE"
-        exit
+    if echo "$startstamp-$endstamp:$level" | grep -E '^[0-9]{12}-[0-9]{12}:(1|2|3|4|5|6)$' >/dev/null
+    then
+      if test "$now" -ge "$startstamp" && test "$now" -le "$endstamp"
+      then
+        if [ -n "$checking" ]
+        then
+          echo "RANGE $startstamp-$endstamp:$level ACTIVE"
+        else
+          echo "$level" >"$output_file"
+          exit
+        fi
+      elif [ -n "$checking" ]
+      then
+        echo "RANGE $startstamp-$endstamp:$level (INACTIVE)"
       fi
     fi
   done
@@ -54,6 +74,7 @@ exceeded_allowed() {
 
 now=$(date +'%Y%m%d%H%M')
 curl -s "$URL" | exceeded_allowed "$now"
-if ! test -f "$OUTPUT_FILE"; then
-  echo "1" >"$OUTPUT_FILE"
+if [ -z "$checking" ] && ! test -f "$output_file"
+then
+  echo "1" >"$output_file"
 fi
