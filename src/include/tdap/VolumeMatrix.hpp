eef63e9633a55e1d7760f61e9e00f5730c88abee
Emmef <the.emmef@gmail.com>
2022-01-23 13:03:56 +0100

    VolumeMatrix: Implementation split, now created Default (allocated) and Fixed variants

--- src/include/tdap/VolumeMatrix.hpp
+++ src/include/tdap/VolumeMatrix.hpp
@@ -31,8 +31,6 @@
 
 namespace tdap {
 
-typedef int *nm;
-
 /**
  * Defines and applies for each output in a set of OUTPUTS outputs what weight,
  * or "volume", is given to each input from a set of INPUT volumes. Outputs and
@@ -44,113 +42,87 @@ typedef int *nm;
  * @tparam OUTPUTS The number of outputs.
  * @tparam ALIGN_BYTES The number of bytes to align to.
  */
-template <typename T, size_t ALIGN_BYTES = 32> class VolumeMatrix {
+template <typename T, class D, size_t ALIGN_BYTES = 32> class VolumeMatrix {
   static_assert(std::is_floating_point<T>::value,
                 "Expecting floating point type parameter");
 
   static_assert(Power2::constant::is(ALIGN_BYTES));
   static_assert((ALIGN_BYTES % sizeof(T)) == 0);
-  static constexpr size_t ALIGN_ELEMENTS = ALIGN_BYTES / sizeof(T);
+
+protected:
+  /**
+   * Returns the number of elements to represent a vector of inputs, where the
+   * size of the elements is a multiple of ALIGN_BYTES.
+   * @param inputs The number of inputs.
+   * @return The number of elements, representing an aligned number of bytes.
+   */
+  static constexpr size_t alignedInputs(size_t inputs) {
+    const size_t ALIGN_ELEMENTS = ALIGN_BYTES / sizeof(T);
+    return ((inputs - 1) / ALIGN_ELEMENTS + 1) * ALIGN_ELEMENTS;
+  }
+
+  static constexpr size_t neededCapacity(size_t inputs, size_t outputs) {
+    return validateGetInputs(inputs, outputs) * (outputs + 1);
+  }
+
   static constexpr T eps = 1e-8;
-
-  static constexpr size_t alignedBlocks(size_t inputs) {
-    return ((inputs - 1) / ALIGN_ELEMENTS + 1);
-  }
-
-  static constexpr size_t roundUp(size_t inputs) {
-    return alignedBlocks(inputs) * ALIGN_ELEMENTS;
-  }
 
   static constexpr T *aligned(T *raw) {
     ptrdiff_t pos = raw - static_cast<T *>(nullptr);
-    ptrdiff_t offs = roundUp(pos) - pos;
+    ptrdiff_t offs = alignedInputs(pos) - pos;
     return raw + offs;
   }
 
   static constexpr size_t validateGetInputs(size_t inputs, size_t outputs) {
-    if (inputs == 0 || outputs == 0) {
-      throw std::invalid_argument("Inputs and outputs must be positive");
+    if (inputs == 0 || outputs == 0 || Count<T>::max() / 2 < inputs ||
+        !Count<T>::product(alignedInputs(inputs), outputs)) {
+      return 0;
+    } else {
+      return inputs;
     }
-    size_t blocks = alignedBlocks(inputs);
-    if (!Count<T>::product(blocks, ALIGN_ELEMENTS)) {
-      throw std::invalid_argument("Number if volumes too big");
+  }
+
+  static constexpr size_t extraUnalignedCapacity() {
+    if (__STDCPP_DEFAULT_NEW_ALIGNMENT__ >= ALIGN_BYTES) {
+      return 0;
     }
-    size_t alignedInputs = blocks * ALIGN_ELEMENTS;
-    if (!Count<T>::product(alignedInputs, outputs)) {
-      throw std::invalid_argument(
-          "Combination of volumes and outputs too large");
-    }
-    return inputs;
+    return ALIGN_BYTES - __STDCPP_DEFAULT_NEW_ALIGNMENT__;
+  }
+
+  inline size_t ins() const {
+    return static_cast<const D *>(this)->getInputs();
+  }
+
+  inline size_t outs() const {
+    return static_cast<const D *>(this)->getOutputs();
+  }
+
+  inline size_t algn_ins() const {
+    return static_cast<const D *>(this)->getAlignedInputs();
   }
 
   inline T *volumes(size_t output) {
-    return std::assume_aligned<ALIGN_BYTES>(vol +
-                                            in_block * ALIGN_ELEMENTS * output);
+    return std::assume_aligned<ALIGN_BYTES>(static_cast<D *>(this)->volumeData() +
+                                            algn_ins() * output);
   }
 
   inline const T *volumes(size_t output) const {
-    return std::assume_aligned<ALIGN_BYTES>(vol + in_block * output);
+    return std::assume_aligned<ALIGN_BYTES>(
+        static_cast<const D *>(this)->volumeData() + algn_ins() * output);
   }
 
-  size_t ins;
-  size_t in_block;
-  size_t outs;
-  T *data;
-  T *vol;
+  inline size_t getCapacity() const {
+    return static_cast<const D *>(this)->capacity();
+  }
 
   void unsafeCopy(const VolumeMatrix &source) const {
-    std::copy(source.vol, source.vol + (in_block * outs * ALIGN_ELEMENTS), vol);
+    std::copy(source.volumes(0), source.vol + (algn_ins() * outs()),
+              volumes(0));
   }
 
 public:
-  VolumeMatrix(size_t inputs, size_t outputs)
-      : ins(validateGetInputs(inputs, outputs)), in_block(alignedBlocks(ins)),
-        outs(outputs),
-        data(new T[in_block * ALIGN_ELEMENTS * (outputs + 1) + 1]),
-        vol(aligned(data)) {
-    // An extra row of input volumes is included to copy non-aligned input
-    // arrays to
-  }
-
-  /**
-   * Creates a volume matrix with the same values als the source, including
-   * epsilon, the minimum representable volume.
-   * @param source The source matrix to copy.
-   */
-  explicit VolumeMatrix(const VolumeMatrix &source)
-      : VolumeMatrix(source.ins, source.outs) {
-    unsafeCopy(source);
-  }
-
-  /**
-   * Creates a volume matrix with the same values als the source, including
-   * epsilon, the minimum representable volume.
-   * @param source The source matrix to copy.
-   */
-  template<size_t ALIGN>
-  explicit VolumeMatrix(const VolumeMatrix<T, ALIGN> &source)
-      : VolumeMatrix(source.ins, source.outs) {
-    unsafeCopy(source);
-  }
-
-  VolumeMatrix(VolumeMatrix &&moved)
-      : ins(moved.ins), in_block(moved.in_block), outs(moved.outs),
-        data(moved.data), vol(moved.vol) {
-    moved.data = nullptr;
-    moved.vol = nullptr;
-  }
-
-  ~VolumeMatrix() {
-    if (data) {
-      delete[] data;
-      data = nullptr;
-    }
-    vol = nullptr;
-  }
-
-  template<size_t ALIGN>
-  bool assign(const VolumeMatrix<T, ALIGN> &source) {
-    if (ins == source.ins &&outs = source.outs) {
+  template <size_t ALIGN> bool assign(const VolumeMatrix<T, D, ALIGN> &source) {
+    if (ins() == source.ins() && outs() == source.outs()) {
       unsafeCopy(source);
       return true;
     }
@@ -165,7 +137,7 @@ public:
    * @param volume The volume for mapped channels, that defaults to unity.
    */
   void identity(T volume = 1.0) noexcept {
-    const size_t MIN_CHANNELS = std::min(ins, outs);
+    const size_t MIN_CHANNELS = std::min(ins(), outs());
     zero();
     T flushedScale = flushToZero(volume);
     for (size_t output = 0; output < MIN_CHANNELS; output++) {
@@ -181,11 +153,11 @@ public:
    * @param volume The volume for mapped channels, that defaults to unity.
    */
   void identityWrapped(T scale = 1.0) noexcept {
-    const size_t MAX_CHANNELS = std::max(ins, outs);
+    const size_t MAX_CHANNELS = std::max(ins(), outs());
     zero();
     T flushedScale = flushToZero(scale);
     for (size_t output = 0; output < MAX_CHANNELS; output++) {
-      volumes(output % outs)[output % ins] = flushedScale;
+      volumes(output % outs())[output % ins()] = flushedScale;
     }
   }
 
@@ -193,8 +165,8 @@ public:
    * sets all volumes to zero.
    */
   void zero() noexcept {
-    T *pT = std::assume_aligned<ALIGN_BYTES>(vol);
-    std::fill(pT, pT + in_block * outs * ALIGN_ELEMENTS, 0);
+    T *pT = std::assume_aligned<ALIGN_BYTES>(volumes(0));
+    std::fill(pT, pT + algn_ins() * outs(), 0);
   }
 
   /**
@@ -203,9 +175,9 @@ public:
    * rather strict).
    */
   void flushAllToZero() noexcept {
-    for (size_t output = 0; output < outs; output++) {
+    for (size_t output = 0; output < outs(); output++) {
       T *p = volumes(output);
-      for (size_t input = 0; input < ins; input++) {
+      for (size_t input = 0; input < ins(); input++) {
         p[input] = flushToZero(p[input]);
       }
     }
@@ -222,7 +194,7 @@ public:
    * @return whether setting volume was successful.
    */
   bool set(size_t output, size_t input, T volume) noexcept {
-    if (output < outs && input < ins) {
+    if (output < outs() && input < ins()) {
       volumes(output)[input] = flushToZero(volume);
       return true;
     }
@@ -238,7 +210,7 @@ public:
    * @return whether setting volume was successful.
    */
   T get(size_t output, size_t input) const noexcept {
-    if (output < outs && input < ins) {
+    if (output < outs() && input < ins()) {
       return volumes(output)[input];
     }
     return Error::setErrorReturn(Error::BOUND, (T)0);
@@ -253,7 +225,7 @@ public:
   void setAll(T volume) noexcept {
     T flushedToZero = flushToZero(volume);
     T *pT = volumes(0);
-    std::fill(pT, pT + in_block * outs * ALIGN_ELEMENTS, flushedToZero);
+    std::fill(pT, pT + algn_ins() * outs(), flushedToZero);
   }
 
   /**
@@ -264,12 +236,12 @@ public:
    * @param coefficients The integration coefficients used.
    */
   void approach(const VolumeMatrix &source,
-                const IntegrationCoefficients<T> &coefficients) noexcept {
-    if (ins != source.ins || outs != source.outs) {
+                const IntegrationCoefficients<T> &coefficients) {
+    if (ins() != source.ins || outs() != source.outs) {
       throw std::invalid_argument("Approach matrix must have same dimensions");
     }
-    for (size_t output = 0; output < outs; output++) {
-      for (size_t input = 0; input < ins; input++) {
+    for (size_t output = 0; output < outs(); output++) {
+      for (size_t input = 0; input < ins(); input++) {
         approachValue(volumes(output)[input], source.volumes(output)[input],
                       coefficients);
       }
@@ -278,12 +250,12 @@ public:
 
   void applyAlignedInputUnsafe(T *__restrict out,
                                const T *__restrict in) const {
-    const size_t block = in_block * ALIGN_ELEMENTS;
-    T *input = std::assume_aligned<ALIGN_ELEMENTS>(in);
+    const size_t block = algn_ins();
+    T *input = std::assume_aligned<ALIGN_BYTES>(in);
     T *v1 = std::assume_aligned<ALIGN_BYTES>(volumes(0));
-    for (size_t output = 0; output < outs; output++, v1 += block) {
+    for (size_t output = 0; output < outs(); output++, v1 += block) {
       out[output] = v1[0] * input[0];
-      for (size_t i = 1; i < ins; i++) {
+      for (size_t i = 1; i < ins(); i++) {
         out[output] += v1[i] * input[i];
       }
     }
@@ -292,8 +264,8 @@ public:
   template <size_t INS, size_t OUTS>
   void applyAlignedInputUnsafeFixed(T *__restrict out,
                                     const T *__restrict in) const {
-    const size_t block = in_block * ALIGN_ELEMENTS;
-    const T *input = std::assume_aligned<ALIGN_ELEMENTS>(in);
+    const size_t block = algn_ins();
+    const T *input = std::assume_aligned<ALIGN_BYTES>(in);
     const T *v1 = std::assume_aligned<ALIGN_BYTES>(volumes(0));
     for (size_t output = 0; output < OUTS; output++, v1 += block) {
       out[output] = v1[0] * input[0];
@@ -315,7 +287,7 @@ public:
   void apply(AlignedArray<T, O, A> &result,
              const AlignedArray<T, I, A> &input) const {
     static_assert((A % ALIGN_BYTES) == 0);
-    if (I != ins || O != outs) {
+    if (I != ins() || O != outs()) {
       throw std::invalid_argument(
           "VolumeMatrix::apply: input and output sizes do not match");
     }
@@ -332,15 +304,15 @@ public:
    */
   template <size_t O, size_t I>
   void apply(std::array<T, O> &result, const std::array<T, I> &input) const {
-    if (I != ins || O != outs) {
+    if (I != ins() || O != outs()) {
       throw std::invalid_argument(
           "VolumeMatrix::apply: input and output sizes do not match");
     }
     if (((input.begin() - static_cast<T *>(nullptr)) % ALIGN_BYTES) == 0) {
       applyAlignedInputUnsafeFixed<I, O>(result.begin(), input);
     } else {
-      std::copy(input.begin(), input.begin() + ins, volumes(outs));
-      applyAlignedInputUnsafeFixed<I, O>(result.begin(), volumes(outs));
+      std::copy(input.begin(), input.begin() + ins(), volumes(outs()));
+      applyAlignedInputUnsafeFixed<I, O>(result.begin(), volumes(outs()));
     }
   }
 
@@ -362,11 +334,11 @@ public:
       return Error::setErrorReturn(Error::NILL);
     }
 
-    if (((inputs - static_cast<T *>(nullptr)) % ALIGN_ELEMENTS) == 0) {
+    if (((inputs - static_cast<T *>(nullptr)) % ALIGN_BYTES) == 0) {
       applyAlignedInputUnsafe(outputs, inputs);
     } else {
-      std::copy(inputs, inputs + ins, volumes(outs));
-      applyAlignedInputUnsafe(outputs, volumes(outs));
+      std::copy(inputs, inputs + ins(), volumes(outs()));
+      applyAlignedInputUnsafe(outputs, volumes(outs()));
     }
     return true;
   }
@@ -384,17 +356,38 @@ private:
   }
 };
 
-template <typename T, size_t ALIGN_BYTES = 32>
-class IntegratedVolumeMatrix : public VolumeMatrix<T, ALIGN_BYTES> {
-  VolumeMatrix<T, ALIGN_BYTES> toFollow;
-  IntegrationCoefficients<T> integration;
+template <typename T, size_t ALIGN_BYTES>
+class DefaultVolumeMatrix
+    : public VolumeMatrix<T, DefaultVolumeMatrix<T, ALIGN_BYTES>, ALIGN_BYTES> {
+  using Super =
+      VolumeMatrix<T, DefaultVolumeMatrix<T, ALIGN_BYTES>, ALIGN_BYTES>;
+  friend Super;
+
+  size_t inputs;
+  size_t outputs;
+  size_t alignedIns;
+  T *data;
+  T *volumes;
+
+  const T* volumeData() const { return volumes; }
+  T* volumeData() { return volumes; }
 
 public:
-  IntegratedVolumeMatrix(size_t inputs, size_t outputs,
-                         double integrationSamples)
-      : VolumeMatrix<T, ALIGN_BYTES>(inputs, outputs),
-        toFollow(inputs, outputs),
-        integration(IntegrationCoefficients<T>(integrationSamples)) {
+  DefaultVolumeMatrix(size_t inputs_, size_t outputs_)
+      : inputs(Super::validateGetInputs(inputs_, outputs_)), outputs(outputs_),
+        alignedIns(Super::alignedInputs(inputs)),
+        data(new T[Super::neededCapacity(inputs, outputs) +
+                   Super::extraUnalignedCapacity()]),
+        volumes(Super::aligned(data)) {}
+
+  /**
+   * Creates a volume matrix with the same values als the source, including
+   * epsilon, the minimum representable volume.
+   * @param source The source matrix to copy.
+   */
+  explicit DefaultVolumeMatrix(const DefaultVolumeMatrix &source)
+      : DefaultVolumeMatrix(source.inputs, source.outputs) {
+    unsafeCopy(source);
   }
 
   /**
@@ -402,10 +395,85 @@ public:
    * epsilon, the minimum representable volume.
    * @param source The source matrix to copy.
    */
-  template<size_t ALIGN>
-  explicit IntegratedVolumeMatrix(const VolumeMatrix<T, ALIGN> &source)
-      : VolumeMatrix<T, ALIGN_BYTES>(source), toFollow(source) {
+  template <size_t ALIGN>
+  explicit DefaultVolumeMatrix(const DefaultVolumeMatrix<T, ALIGN> &source)
+      : DefaultVolumeMatrix(source.inputs, source.outputs) {
+    unsafeCopy(source);
   }
+
+  DefaultVolumeMatrix(DefaultVolumeMatrix &&moved)
+      : inputs(moved.inputs), alignedIns(moved.alignedIns),
+        outputs(moved.outputs), data(moved.data), volumes(moved.volumes) {
+    moved.data = nullptr;
+    moved.volumes = nullptr;
+  }
+
+  ~DefaultVolumeMatrix() {
+    if (data) {
+      delete[] data;
+      data = nullptr;
+    }
+    volumes = nullptr;
+  }
+  size_t getInputs() const { return inputs; }
+  size_t getAlignedInputs() const { return alignedIns; }
+  size_t getOutputs() const { return outputs; }
+};
+
+template <typename T, size_t INPUTS, size_t OUTPUTS, size_t ALIGN_BYTES>
+class FixedVolumeMatrix
+    : public VolumeMatrix<T, FixedVolumeMatrix<T, INPUTS, OUTPUTS, ALIGN_BYTES>,
+                          ALIGN_BYTES> {
+  using Super =
+      VolumeMatrix<T, FixedVolumeMatrix<T, INPUTS, OUTPUTS, ALIGN_BYTES>,
+                   ALIGN_BYTES>;
+  friend Super;
+
+  static_assert(Super::validateGetInputs(INPUTS, OUTPUTS) > 0);
+  static constexpr size_t ALIGNED_INPUTS = Super::alignedInputs(INPUTS);
+  static constexpr size_t CAPACITY = Super::neededCapacity(INPUTS, OUTPUTS);
+
+  alignas(ALIGN_BYTES) T volumes[CAPACITY];
+  const T* volumeData() const { return &volumes[0]; }
+  T* volumeData() { return &volumes[0]; }
+
+public:
+  FixedVolumeMatrix() {}
+
+  /**
+   * Creates a volume matrix with the same values als the source, including
+   * epsilon, the minimum representable volume.
+   * @param source The source matrix to copy.
+   */
+  explicit FixedVolumeMatrix(const FixedVolumeMatrix &source) = default;
+  FixedVolumeMatrix(FixedVolumeMatrix &&source) noexcept = default;
+
+  size_t getInputs() const { return INPUTS; }
+  size_t getAlignedInputs() const { return ALIGNED_INPUTS; }
+  size_t getOutputs() const { return OUTPUTS; }
+};
+
+template <typename T, class D, size_t ALIGN_BYTES = 32>
+class IntegratedVolumeMatrix : public VolumeMatrix<T, D, ALIGN_BYTES> {
+  static_assert(std::is_base_of_v<VolumeMatrix<T, D, ALIGN_BYTES>, D>);
+  VolumeMatrix<T, D, ALIGN_BYTES> toFollow;
+  IntegrationCoefficients<T> integration;
+
+public:
+  IntegratedVolumeMatrix(size_t inputs, size_t outputs,
+                         double integrationSamples)
+      : VolumeMatrix<T, D, ALIGN_BYTES>(inputs, outputs),
+        toFollow(inputs, outputs),
+        integration(IntegrationCoefficients<T>(integrationSamples)) {}
+
+  /**
+   * Creates a volume matrix with the same values als the source, including
+   * epsilon, the minimum representable volume.
+   * @param source The source matrix to copy.
+   */
+  template <size_t ALIGN>
+  explicit IntegratedVolumeMatrix(const VolumeMatrix<T, D, ALIGN> &source)
+      : VolumeMatrix<T, D, ALIGN_BYTES>(source), toFollow(source) {}
 
   IntegratedVolumeMatrix(IntegratedVolumeMatrix &&moved) = default;
 
@@ -413,14 +481,11 @@ public:
     integration.setCharacteristicSamples(integrationSamples);
   }
 
-  template <size_t ALIGN>
-  bool assign(const VolumeMatrix<T, ALIGN> *source) {
+  template <size_t ALIGN> bool assign(const VolumeMatrix<T, D, ALIGN> *source) {
     return toFollow.template assign(source);
   }
 
-  void approach() {
-    approach(toFollow, integration);
-  }
+  void approach() { approach(toFollow, integration); }
 };
 
 } // namespace tdap
