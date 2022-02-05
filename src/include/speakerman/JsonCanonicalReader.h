1fb05cef5fc3634be7870c0e0f0a03c39c2c69a3
Emmef <the.emmef@gmail.com>
2022-02-05 22:08:06 +0100

    Working on JSON parsing

---
+++ src/include/speakerman/JsonCanonicalReader.h
@@ -0,0 +1,136 @@
+#ifndef SPEAKERMAN_M_JSON_CANONICAL_READER_H
+#define SPEAKERMAN_M_JSON_CANONICAL_READER_H
+/*
+ * speakerman/JsonCanonicalReader.h
+ *
+ * Added by michel on 2022-02-05
+ * Copyright (C) 2015-2022 Michel Fleur.
+ * Source https://github.com/emmef/speakerman
+ * Email speakerman@emmef.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <algorithm>
+#include <cstddef>
+#include <cstring>
+#include <istream>
+#include <org-simple/util/text/Json.h>
+
+namespace speakerman {
+
+class PartitionBasedJsonStringBuilder
+    : public org::simple::util::text::JsonStringBuilder {
+  char *start = nullptr;
+  char *rendered = nullptr;
+  char *last  = nullptr;
+  char *local = nullptr;
+  char *at = nullptr;
+
+  static size_t validLength(size_t length);
+
+protected:
+  const char *getValue() const final;
+  bool addChar(const char &c) final;
+
+public:
+  explicit PartitionBasedJsonStringBuilder(size_t maxLength);
+
+  size_t getLength() const final;
+
+  void resetValue() final;
+
+  const char *getTotalString(char separator) const;
+
+  void setLocal(char *newValue);
+
+  char *getAt() const;
+  char *getLocal() const;
+
+  ~PartitionBasedJsonStringBuilder();
+};
+
+class JsonCanonicalReader : protected org::simple::util::text::JsonContext {
+  struct StackEntry {
+    char *start = nullptr;
+  };
+
+  static constexpr size_t validDepth(size_t depth);
+
+  StackEntry *stack;
+  StackEntry *stackLast;
+  StackEntry *stackAt = nullptr;
+  PartitionBasedJsonStringBuilder path;
+  PartitionBasedJsonStringBuilder value;
+
+  void checkPush() const;
+
+  void checkPop() const;
+
+protected:
+  org::simple::util::text::JsonStringBuilder &nameBuilder() final;
+
+  org::simple::util::text::JsonStringBuilder &stringBuilder() final;
+
+  void pushIndex(int index) final;
+
+  void popIndex() final;
+
+  void pushName(const char *name) final;
+
+  void popName() final;
+
+  void setString(const char *string) final;
+
+  void setNumber(const char *string) final;
+
+  void setBoolean(bool value) final;
+
+  void setNull() final;
+
+public:
+  JsonCanonicalReader(size_t pathLength, size_t valueLength,
+                                       size_t depth);
+
+  virtual void setString(const char *path, const char *string) = 0;
+
+  virtual void setNumber(const char *path, const char *string) = 0;
+
+  virtual void setBoolean(const char *path, bool value) = 0;
+
+  virtual void setNull(const char *path) = 0;
+
+  void readJson(org::simple::util::text::InputStream<char> &input) {
+    JsonContext::readJson(*this, input);
+  }
+
+  void readJson(std::istream &input) {
+    class Stream : public org::simple::util::text::InputStream<char> {
+      std::istream &input;
+    public:
+      Stream(std::istream &in) : input(in) {}
+      bool get(char &c) final {
+        if (!input.eof()) {
+          input.get(c);
+          return true;
+        }
+        return false;
+      }
+    } stream(input);
+
+    JsonContext::readJson(*this, stream);
+  }
+};
+
+} // namespace speakerman
+
+#endif // SPEAKERMAN_M_JSON_CANONICAL_READER_H
---
+++ test/TestJsonCanonicalReader.cc
@@ -0,0 +1,164 @@
+//
+// Created by michel on 05-02-22.
+//
+#include "boost-unit-tests.h"
+#include <org-simple/util/text/StringStream.h>
+#include <speakerman/JsonCanonicalReader.h>
+
+using StringStream = org::simple::util::text::StringInputStream<char>;
+
+struct Setter {
+  std::string path;
+  std::string value;
+
+  Setter(const std::string p, const std::string v) : path(p), value(v) {}
+
+  bool operator==(const Setter &s) const {
+    return this == &s || (path == s.path && value == s.value);
+  }
+};
+
+std::ostream &operator<<(std::ostream &out, const Setter &setter) {
+  out << "{" << setter.path << " = " << setter.value << "}";
+  return out;
+}
+
+std::ostream &operator<<(std::ostream &out, const std::vector<Setter> &list) {
+  out << "\t{" << std::endl;
+  for (const Setter &setter : list) {
+    out << "\t\t" << setter << std::endl;
+  }
+  out << "\t}" << std::endl;
+  return out;
+}
+
+class CanonicalReader : speakerman::JsonCanonicalReader {
+  std::vector<Setter> actualOutput;
+
+public:
+  CanonicalReader() : speakerman::JsonCanonicalReader(128, 128, 10) {}
+
+  const std::vector<Setter> &run(const std::string &input) {
+    actualOutput.clear();
+    org::simple::util::text::StringInputStream<char> stream(input);
+    this->readJson(stream);
+    return actualOutput;
+  }
+
+  void setString(const char *path, const char *string) final {
+    std::string key = "String ";
+    key += path;
+    actualOutput.push_back({key, string});
+  }
+
+  void setNumber(const char *path, const char *string) final {
+    std::string key = "Number ";
+    key += path;
+    actualOutput.push_back({key, string});
+  }
+
+  void setBoolean(const char *path, bool value) final {
+    std::string key = "Boolean ";
+    key += path;
+    actualOutput.push_back({key, (value ? "true" : "false")});
+  }
+
+  void setNull(const char *path) final {
+    std::string key = "Null ";
+    key += path;
+    actualOutput.push_back({key, ""});
+  }
+
+  const std::vector<Setter> &getVector() const { return actualOutput; }
+};
+
+class Scenario {
+  std::string input;
+  std::vector<Setter> expectedOutput;
+
+public:
+  Scenario(std::string string, std::initializer_list<Setter> expected)
+      : input(string), expectedOutput(expected) {}
+
+  void writeTo(std::ostream &out) const {
+    out << "Actual{" << std::endl;
+    out << "\t" << input << std::endl;
+    out << " =>" << std::endl;
+    out << expectedOutput;
+    out << '}';
+  }
+
+  const std::string getInput() const { return input; }
+
+  void test() const {
+    CanonicalReader reader;
+    const auto &actual = reader.run(input);
+    BOOST_CHECK_EQUAL(expectedOutput.size(), actual.size());
+    for (size_t i = 0; i < expectedOutput.size(); i++) {
+      BOOST_CHECK_EQUAL(expectedOutput.at(i), actual.at(i));
+    }
+  }
+};
+
+std::ostream &operator<<(std::ostream &out, const Scenario &reader) {
+  reader.writeTo(out);
+  return out;
+}
+
+std::vector<Scenario> generateScenerios() {
+  std::vector<Scenario> result;
+
+  result.push_back({R"(
+    {
+      "name1" : true,
+      "name2" : 13
+    })",
+                    {{"Boolean name1", "true"}, {"Number name2", "13"}}});
+
+  result.push_back(
+      {R"(
+    {
+      "name1" : [
+        {"name2" : 13},
+        {"name3" : 14 }
+      ]
+    })",
+       {{"Number name1/0/name2", "13"}, {"Number name1/1/name3", "14"}}});
+
+  result.push_back(
+      {R"(
+      {
+        "name1" : [
+          {"name2" : 13},
+          {"name2" : 14 }
+        ]
+      })",
+       {{"Number name1/0/name2", "13"}, {"Number name1/1/name2", "14"}}});
+
+  result.push_back(
+      {R"(
+        {
+          "name1" : [
+            {"name2" : null},
+            {"name2" : 16 }
+          ]
+        })",
+       {{"Null name1/0/name2", ""}, {"Number name1/1/name2", "16"}}});
+
+  result.push_back({R"(
+        {
+          "name1" : {
+            "name2" : null,
+            "name2" : 16
+          }
+        })",
+                    {{"Null name1/name2", ""}, {"Number name1/name2", "16"}}});
+
+  return result;
+}
+
+BOOST_AUTO_TEST_SUITE(test_speakerman_TestJsonCanonicalReader)
+
+BOOST_DATA_TEST_CASE(testBasicScenarios, generateScenerios()) { sample.test(); }
+
+BOOST_AUTO_TEST_SUITE_END()
